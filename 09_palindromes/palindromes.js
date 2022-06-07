097089306c0c738ecce3c46ae9d828a475472483
Adam Nagy <nagy.adamn@gmail.com>
2022-06-07 16:20:57 +0200

    Solve 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+	const original = sentence.replace(/\W/g, "").toLowerCase();
+	const reversed = original.split("").reverse().join("");
+	return reversed === original;
 };
 
 // Do not edit below this line
