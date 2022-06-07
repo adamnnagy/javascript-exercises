d53e000e9a9108e3637f88099112a3410c0a81b5
Adam Nagy <nagy.adamn@gmail.com>
2022-06-07 14:10:05 +0200

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...toRemove) {
+	const result = arr.sort();
+	toRemove.forEach((el) => {
+		const indexToRemove = result.indexOf(el);
+		if (indexToRemove !== -1) result.splice(indexToRemove, 1);
+	});
+	return result;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
