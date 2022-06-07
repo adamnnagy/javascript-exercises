d6015a1185afcee20cf5778ad78307454a79e1f4
Adam Nagy <nagy.adamn@gmail.com>
2022-06-07 16:04:27 +0200

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+	return Number(numbers.reduce((acc, curr) => acc + curr, 0));
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+	return Number(numbers.reduce((acc, curr) => acc * curr, 1));
 };
 
-const power = function() {
-	
+const power = function (x, n) {
+	if (x < 0) return "ERROR";
+	if (n === 0) return 1;
+	else return x * power(x, n - 1);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+	if (x < 0) return "ERROR";
+	if (x === 0) return 1;
+	else return x * factorial(x - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
