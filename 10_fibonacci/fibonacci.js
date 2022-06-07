0bb281be6e3270d89de950d0b8bd99d6d903f450
Adam Nagy <nagy.adamn@gmail.com>
2022-06-07 16:35:03 +0200

    Solve Fibo - 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    const nthFibonacci = Number(n);
+	if (nthFibonacci < 1) return "OOPS";
+	if (nthFibonacci === 1 || nthFibonacci === 2) return 1;
 
+	let curr = 1;
+	let next = 1;
+	let prev = 1;
+	for (let i = 0; i < nthFibonacci - 2; i++) {
+		next = prev + curr;
+		prev = curr;
+		curr = next;
+	}
+	return next;
 };
 
 // Do not edit below this line
