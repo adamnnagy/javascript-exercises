60dfed3fbb0d801573aae131d6ad80134ca66281
Adam Nagy <nagy.adamn@gmail.com>
2022-06-07 14:48:07 +0200

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+	const celsius = (fahrenheit - 32) / 1.8;
+	return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+	const fahrenheit = (celsius * 1.8) + 32;
+	return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
