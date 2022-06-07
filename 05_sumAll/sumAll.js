58de6e3dc98ad8b6694c6f4c07849a2e9323a5b3
Adam Nagy <nagy.adamn@gmail.com>
2022-06-07 14:32:42 +0200

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (from, until) {
+	if (from < 0 || until < 0) return "ERROR";
+	if ((typeof from !== 'number') || (typeof until !== 'number')) return "ERROR";
+	const n = Math.abs(until - from) + 1;
+	const sum = (n * (from + until)) / 2;
+	return sum;
 };
 
 // Do not edit below this line
