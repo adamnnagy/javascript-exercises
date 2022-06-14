826557efe4d4e428149ea57ba1ab8a5b16f4f7c0
Adam Nagy <nagy.adamn@gmail.com>
2022-06-14 17:03:13 +0200

    Solve pigLatin

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,6 +1,18 @@
 function pigLatin(string) {
+	if (!string) return "error";
+	let finalResult = "";
+	const pigLatinWord = (s) => {
+		if (s[0].match(/^[aeiouAEIOU]/)) {
+			return s + "ay";
+		} else {
+			return s.replace(/(^[bcdfghjklmnprstvwxys]+qu|qu|[bcdfghjklmnpqrstvwxys]+)(\w*)/, "$2$1ay");
+		}
+	};
+	string.split(" ").forEach((word) => {
+		finalResult += `${pigLatinWord(word)} `;
+	});
+	return finalResult.slice(0, finalResult.length - 1);
+}
 
-};
-  
 // Do not edit below this line
 module.exports = pigLatin;
